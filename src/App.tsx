ca44c46f4413d5d7a57de6f7bf6db5a1a922ab30
Borut Balazek <bobalazek124@gmail.com>
2023-03-27 11:36:47 +0200

    Have only 1 navigator in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import NetInfo from '@react-native-community/netinfo';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useEffect } from 'react';
 import Toast from 'react-native-toast-message';
 
-import AuthNavigator from './features/auth/navigators/AuthNavigator';
+import LoginScreen from './features/auth/screens/LoginScreen';
+import RegisterScreen from './features/auth/screens/RegisterScreen';
 import { isAuthenticatedSelector } from './features/auth/state/authReducer';
-import HomeNavigator from './features/home/navigators/HomeNavigator';
+import DashboardScreen from './features/home/screens/DashboardScreen';
+import SettingsScreen from './features/settings/screens/SettingsScreen';
 import { useAppDispatch, useAppSelector } from './hooks';
 import { setConnectionType, setIsConnected } from './state/networkReducer';
+
+export type RootStackParams = {
+  Login: undefined;
+  Register: undefined;
+  Dashboard: undefined;
+  Settings: undefined;
+};
+
+export const RootStack = createNativeStackNavigator<RootStackParams>();
 
 const App = () => {
   const dispatch = useAppDispatch();
@@ -31,7 +43,19 @@ const App = () => {
 
   return (
     <>
-      {isAuthenticated ? <HomeNavigator /> : <AuthNavigator />}
+      <RootStack.Navigator>
+        {isAuthenticated ? (
+          <RootStack.Group>
+            <RootStack.Screen name="Dashboard" component={DashboardScreen} />
+            <RootStack.Screen name="Settings" component={SettingsScreen} />
+          </RootStack.Group>
+        ) : (
+          <RootStack.Group screenOptions={{ headerShown: false }}>
+            <RootStack.Screen name="Login" component={LoginScreen} />
+            <RootStack.Screen name="Register" component={RegisterScreen} options={{ title: 'Sign up' }} />
+          </RootStack.Group>
+        )}
+      </RootStack.Navigator>
       <Toast />
     </>
   );
